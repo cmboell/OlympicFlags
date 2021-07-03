11c24265e67e5cc3dfe50a20cc5532a1faa81e04
Colby Boell <cmboell1221@gmail.com>
2021-07-03 17:55:25 -0500

    fixed bugs

--- OlympicFlags/Controllers/HomeController.cs
+++ OlympicFlags/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OlympicFlags.Models;
@@ -9,6 +11,7 @@ namespace OlympicFlags.Controllers
 {
     public class HomeController : Controller
     {
+
         private CountryContext context;
 
         public HomeController(CountryContext ctx)
@@ -18,7 +21,17 @@ namespace OlympicFlags.Controllers
 
         public IActionResult Index(CountryListViewModel model)
         {
-            var data = new CountryListViewModel
+            model.Categories = context.Categories.ToList();
+            model.Games = context.Games.ToList();
+            model.Sports = context.Sports.ToList();
+
+            var session = new OlympicSession(HttpContext.Session);
+            session.SetActiveCategory(model.ActiveCategory);
+            session.SetActiveGame(model.ActiveGame);
+            session.SetActiveSport(model.ActiveSport);
+            // if no count value in session, use data in cookie to restore fave teams in session 
+            int? count = session.GetMyTeamCount();
+            if (count == null)
             {
                 var cookies = new OlympicCookies(HttpContext.Request.Cookies);
                 string[] ids = cookies.GetMyTeamIds();
@@ -33,10 +46,13 @@ namespace OlympicFlags.Controllers
             }
 
             IQueryable<Country> query = context.Countries;
-            if (ActiveCategory != "all")
+            if (model.ActiveCategory != "all")
                 query = query.Where(
-                    t => t.Category.CategoryId.ToLower() == ActiveCategory.ToLower());
-            if (ActiveGame != "all")
+                    t => t.Category.CategoryId.ToLower() == model.ActiveCategory.ToLower());
+            if (model.ActiveGame != "all")
+                query = query.Where(
+                    t => t.Game.GameId.ToLower() == model.ActiveGame.ToLower());
+            if (model.ActiveSport != "all")
                 query = query.Where(
                     t => t.Sport.SportId.ToLower() == model.ActiveSport.ToLower());
             model.Countries = query.ToList();
@@ -44,7 +60,6 @@ namespace OlympicFlags.Controllers
             return View(model);
         }
 
-        [HttpGet]
         public IActionResult Details(string id)
         {
             var session = new OlympicSession(HttpContext.Session);
@@ -59,7 +74,36 @@ namespace OlympicFlags.Controllers
                 ActiveGame = session.GetActiveGame(),
                 ActiveCategory = session.GetActiveCategory()
             };
-            return View(model); //displays view 
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Add(CountryViewModel model)
+        {
+            model.Country = context.Countries
+                .Include(t => t.Category)
+                .Include(t => t.Game)
+                .Include(t => t.Sport)
+                .Where(t => t.CountryId == model.Country.CountryId)
+                .FirstOrDefault();
+
+            var session = new OlympicSession(HttpContext.Session);
+            var countries = session.GetMyTeams();
+            countries.Add(model.Country);
+            session.SetMyTeams(countries);
+
+            var cookies = new OlympicCookies(HttpContext.Response.Cookies);
+            cookies.SetMyTeamIds(countries);
+
+            TempData["message"] = $"{model.Country.Name} added to your favorites";
+
+            return RedirectToAction("Index",
+                new
+                {
+                    ActiveSport = session.GetActiveSport(),
+                    ActiveGame = session.GetActiveGame(),
+                    ActiveCategory = session.GetActiveCategory()
+                });
         }
     }
 }
